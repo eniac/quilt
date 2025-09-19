2643528ad6870641b584899f1ac5a594ebf0f8d9
zyuxuan <zyuxuan@localhost>
2025-09-18 21:29:08 -0400

    update

---
+++ merge_func/merge-c-and-rust/example/caller/build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+FUNC=caller-c
+
+function build {
+  sudo docker build --no-cache -t zyuxuan0115/$FUNC:latest -f Dockerfile . 
+  sudo docker push zyuxuan0115/$FUNC:latest
+}
+
+function deploy {
+  fission function run-container --name $FUNC \
+    --image docker.io/zyuxuan0115/$FUNC:latest \
+    --minscale=1 --maxscale=30 \
+    --minmemory=1 --maxmemory=64 \
+    --mincpu=1  --maxcpu=8000 \
+    --port 8888 \
+    --namespace fission-function
+  fission httptrigger create --method POST \
+    --url /$FUNC --function $FUNC \
+    --namespace fission-function
+}
+
+function invoke {
+  curl -XPOST http://localhost:8888/$FUNC \
+  -d '{"iter_count":12}'
+}
+
+case "$1" in
+build)
+    build
+    ;;
+deploy)
+    deploy
+    ;;
+invoke)
+    invoke
+    ;;
+esac
