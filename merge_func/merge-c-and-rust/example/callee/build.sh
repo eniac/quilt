c7829b26e68f6a102b32799260a6b8e177abc254
zyuxuan <zyuxuan@localhost>
2025-09-20 16:36:04 -0400

    update

---
+++ merge_func/merge-c-and-rust/example/callee/build.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+ROOT_DIR=`realpath $(dirname $0)`
+DOCKERFILE_DIR=`realpath $(dirname $0)/../../../../../dockerfiles`
+FUNC=callee-rust
+PARENT_DIR=`realpath $(dirname $0)/..`
+CLUSTER_ID="${PARENT_DIR: -1}"
+USERNAME=$(echo $DOCKER_USER)
+
+function build_fission_container {
+  sudo docker build --no-cache -t $USERNAME/$FUNC:latest \
+    --build-arg USERNAME=$USERNAME \
+    -f Dockerfile \
+    .
+  sudo docker system prune -f
+  sudo docker push $USERNAME/$FUNC:latest
+}
+
+function deploy_fission_c {
+  MINCPU=1
+  MAXCPU=1000
+  MINMEM=1
+  MAXMEM=64
+  MINSCALE=1
+  MAXSCALE=3
+
+  fission function run-container --name $FUNC \
+    --image docker.io/$USERNAME/$FUNC --port 8888 \
+    --minscale=$MINSCALE --maxscale=$MAXSCALE \
+    --minmemory=$MINMEM --maxmemory=$MAXMEM \
+    --mincpu=$MINCPU  --maxcpu=$MAXCPU \
+    --secret tracing \
+    --namespace fission-function
+  fission httptrigger create --method POST \
+    --url /$FUNC --function $FUNC \
+    --namespace fission-function
+}
+
+
+function delete_fission {
+  fission function delete --name $FUNC \
+    -n fission-function
+  fission httptrigger delete --function $FUNC \
+    -n fission-function
+}
+
+case "$1" in
+build)
+    build_fission_container
+    ;;
+deploy)
+    deploy_fission_c
+    ;;
+delete)
+    delete_fission
+    ;;
+esac
