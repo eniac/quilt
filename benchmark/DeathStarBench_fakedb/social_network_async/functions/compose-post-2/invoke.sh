cf0e823877275ed1d2087d6e26b78b78412b0772
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2025-10-03 19:07:17 -0400

    update

--- benchmark/DeathStarBench_fakedb/social_network_async/functions/compose-post-2/invoke.sh
+++ benchmark/DeathStarBench_fakedb/social_network_async/functions/compose-post-2/invoke.sh
@@ -4,7 +4,7 @@ FUNC=compose-post-2
 
 function invoke_fission {
   curl -XPOST http://localhost:8888/$FUNC \
-  -d '{"username":"username_888","user_id":888,"text":"Hey, this is @username_888! Nice to meet you and welcome to my personal web: https://eecs.umich.edu/~twenisch @username_999 ","media_ids":[],"media_types":[],"post_type":"POST"}'
+  -d '{"username":"username_888","user_id":888,"text":"Hey, this is @username_888! Nice to meet you and welcome to my personal web: https://eecs.umich.edu/~tomw @username_999 ","media_ids":[],"media_types":[],"post_type":"POST"}'
 }
 
 case "$1" in
