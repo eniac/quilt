ff0439b8f60323821069fe3b9f14f4c39786d378
zyuxuan <zyuxuan@localhost>
2025-10-04 16:38:59 -0400

    update

--- merge_func/merge-rust-and-c/example/merge_script/merge_local.sh
+++ merge_func/merge-rust-and-c/example/merge_script/merge_local.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 LLVM_DIR=/proj/zyuxuanssf-PG0/zyuxuan/llvm-project-pthread/build/bin
-RUST_LIB=/users/zyuxuan/.rustup/toolchain/1.76-x86_64-unknown-linux-gnu/lib
+RUST_LIB=/users/zyuxuan/.rustup/toolchains/1.76-x86_64-unknown-linux-gnu/lib
 LIBSTD="$(printf '%s\n' "$RUST_LIB"/libstd*.so | head -n 1 || true)"
 
 base="${LIBSTD##*/}"     # e.g., libstd-66d8041607d2929b.so
@@ -9,21 +9,23 @@ base="${base%.so}"     # e.g., libstd-66d8041607d2929b
 LIBSTD_BASENAME="${base:0:1}${base:3}"   # e.g., lstd-66d8041607d2929b
 LINKER_FLAGS="-$LIBSTD_BASENAME -lcurl -lcrypto -lm -lssl"
 
+echo $LIBSTD_BASENAME
+
 function merge {
   cd ../caller \
   && RUSTFLAGS="--emit=llvm-ir" cargo build
   cd ../callee \
-  && $LLVM_DIR/clang -fPIC -emit-llvm -S callee.c -c -o callee.ll \
-  && cd ../wrapper && RUSTFLAGS="--emit=llvm-ir" cargo build \
+  && $LLVM_DIR/clang -fPIC -emit-llvm -S callee.c -c -o callee.ll
+  cd ../wrapper && RUSTFLAGS="--emit=llvm-ir" cargo build \
   && cd ../merge_script
 
-  $LLVM_DIR/opt -S callee/callee.ll -passes=rename-func --callee-lang=c -o callee_rename.ll
-  cp callee_rename.ll caller/target/debug/deps/
-  $LLVM_DIR/opt -S wrapper/target/debug/deps/wrapper-c0f1fa4e8eb016c4.ll -passes=rename-func --callee-lang=rust -o wrapper_rename.ll
-  cp wrapper_rename.ll caller/target/debug/deps/
-  cp wrapper/target/debug/deps/*.ll caller/target/debug/deps/
-  rm caller/target/debug/deps/wrapper-c0f1fa4e8eb016c4.ll
-  $LLVM_DIR/llvm-link caller/target/debug/deps/*.ll -S -o merge.ll
+  $LLVM_DIR/opt -passes=merge-rust-c-func --rename-callee-rc="c" -S ../callee/callee.ll -o callee_rename.ll
+  cp callee_rename.ll ../caller/target/debug/deps/
+  $LLVM_DIR/opt -S ../wrapper/target/debug/deps/wrapper-*.ll -passes=merge-rust-c-func --rename-callee-rc="rust" -o wrapper_rename.ll
+  cp wrapper_rename.ll ../caller/target/debug/deps/
+  cp ../wrapper/target/debug/deps/*.ll ../caller/target/debug/deps/
+  rm ../caller/target/debug/deps/wrapper-*.ll
+  $LLVM_DIR/llvm-link ../caller/target/debug/deps/*.ll -S -o merge.ll
   $LLVM_DIR/opt merge.ll -strip-debug -o merge_nodebug.ll -S
   $LLVM_DIR/opt -S merge_nodebug.ll -passes=merge-rust-c-func -o merge_new.ll
   $LLVM_DIR/llc -filetype=obj merge_new.ll -o function.o
@@ -31,10 +33,10 @@ function merge {
 }
 
 function clean {
-  cd caller && cargo clean \
-  && cd ../callee && rm *.ll \
-  && cd ../wrapper && cargo clean \
-  && cd ..
+  cd ../caller && cargo clean
+  cd ../callee && rm *.ll
+  cd ../wrapper && cargo clean
+  cd ../merge_script
   rm -rf *.ll *.o function
 }
 
