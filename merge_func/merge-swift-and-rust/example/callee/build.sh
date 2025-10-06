be850a0d955252480c6dd0140bd35c7cc009965d
zyuxuan <zyuxuan@localhost>
2025-10-06 14:27:48 -0400

    update

--- merge_func/merge-swift-and-rust/example/callee/build.sh
+++ merge_func/merge-swift-and-rust/example/callee/build.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
+USERNAME=$(echo $DOCKER_USER)
+FUNC=rust-callee
+
 function build {
-  sudo docker build --no-cache  -t zyuxuan0115/rust-callee:latest -f Dockerfile . 
-  sudo docker push zyuxuan0115/rust-callee:latest
+  sudo docker build --no-cache  \
+    -t $USERNAME/$FUNC:latest \
+    --build-arg USERNAME=$USERNAME \
+    -f Dockerfile . 
+  sudo docker push $USERNAME/$FUNC:latest
 }
 
 function deploy {
-  fission function run-container --name rust-callee \
-    --image docker.io/zyuxuan0115/rust-callee:latest \
+  fission function run-container --name $USERNAME \
+    --image docker.io/$USERNAME/$FUNC:latest \
     --port 8888 \
     --namespace fission-function
   fission httptrigger create --method POST \
-    --url /rust-callee --function rust-callee \
+    --url /$FUNC --function $FUNC \
     --namespace fission-function
 }
 
 function invoke {
-  curl -XPOST http://localhost:8888/rust-callee \
+  curl -XPOST http://localhost:8888/$USERNAME \
   -d 'abcdefg'
 }
 
